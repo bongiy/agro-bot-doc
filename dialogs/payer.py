e15fce9c230d60cb90d01f776e1a24567aa74be7
bongiy <bongiy11@gmail.com>
2025-07-23 09:50:32 +0300

    Update payer.py

--- dialogs/payer.py
+++ dialogs/payer.py
@@ -571,7 +571,7 @@ add_payer_conv = ConversationHandler(
         IDCARD_ISSUER: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_payer_idcard_issuer)],
         IDCARD_DATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_payer_idcard_date)],
         BIRTH_DATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_payer_birth_date)],
-        EDIT_SELECT: [CallbackQueryHandler(edit_field_input, pattern=r"^edit_field:")],
+        EDIT_SELECT: [CallbackQueryHandler(edit_field_input, pattern=r"^edit_field:\d+:\w+$")],
         EDIT_VALUE: [MessageHandler(filters.TEXT & ~filters.COMMAND, edit_field_save)],
 ConversationHandler.TIMEOUT: [],
     },
