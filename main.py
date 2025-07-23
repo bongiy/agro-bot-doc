5f29f4076210dd185ec8a505e7451d12a9a74c9e
bongiy <bongiy11@gmail.com>
2025-07-23 12:32:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters
 from dialogs.payer import (
     menu_keyboard, add_payer_conv, show_payers, payer_card, delete_payer,
-    payer_search_start, payer_search_do, create_contract, to_menu, edit_payer_menu
+    payer_search_start, payer_search_do, create_contract, to_menu, edit_payer_menu, edit_field_input,
 )
 from db import database
 
