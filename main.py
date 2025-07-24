e83cf41bddbfcacfb7d14d272a5486922a554414
bongiy <bongiy11@gmail.com>
2025-07-24 09:59:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from dialogs.payer import (
 from dialogs.edit_payer import edit_payer_conv
 from dialogs.search import search_payer_conv
 from dialogs.field import add_field_conv, show_fields, delete_field, to_fields_list
-from dialogs.land import add_land_conv, show_lands, delete_land, to_lands_list
+from dialogs.land import add_land_conv, show_lands, land_card, delete_land, to_lands_list, edit_land
 
 from db import database
 
