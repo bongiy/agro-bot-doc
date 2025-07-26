f25c6fa22993c9ecf86a38eac64eca7486881892
bongiy <bongiy11@gmail.com>
2025-07-26 23:49:52 +0300

    Update field.py

--- dialogs/field.py
+++ dialogs/field.py
@@ -96,17 +96,16 @@ async def field_card(update, context):
     )])
 
 # Кнопки перегляду/видалення PDF для поля з назвою типу документа
-    docs = await database.fetch_all(
+  docs = await database.fetch_all(
     sqlalchemy.select(UploadedDocs)
     .where((UploadedDocs.c.entity_type == "field") & (UploadedDocs.c.entity_id == field_id))
 )
-    for doc in docs:
+for doc in docs:
     doc_type = doc['doc_type']
     kb.append([
         InlineKeyboardButton(f"⬇️ {doc_type}", callback_data=f"send_pdf:{doc['id']}"),
         InlineKeyboardButton(f"🗑 Видалити", callback_data=f"delete_pdf_db:{doc['id']}")
     ])
-
 
     # Інші кнопки
     kb.extend([
